c1bc5fed59935c557450f41aa14d265c80e5165c
Jungwook <jungwook.l.92@gmail.com>
2019-10-28 15:17:43 -0400

    Add twosum

---
+++ python/leetcode/arrays/twosum.py
@@ -0,0 +1,26 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    diff = dict()
+    for i in range(len(nums)):
+        diff[target - nums[i]] = i
+    for j in range(len(nums)):
+        if nums[j] in diff and diff[nums[j]] != j:
+            return [j, diff[nums[j]]]
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    diff = dict()
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if nums[i] in diff:
+            return [i, diff[nums[i]]]
+        diff[complement] = i
+        
