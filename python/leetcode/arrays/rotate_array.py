d57a6c4e8c4c09d21cc3fbadf52c1c50eacf15f4
Jungwook <jungwook.l.92@gmail.com>
2020-01-16 15:25:22 -0500

    Add reverse solution

--- python/leetcode/arrays/rotate_array.py
+++ python/leetcode/arrays/rotate_array.py
@@ -8,13 +8,25 @@
 #     print(nums)
 
 # O(n), but O(n) space
-def rotate_array(nums, k):
-    out = [0] * len(nums)
-    for i in range(len(nums)):
-        new_i = (i + k) % len(nums)
-        out[new_i] = nums[i]
-    print(out)
+# def rotate_array(nums, k):
+#     out = [0] * len(nums)
+#     for i in range(len(nums)):
+#         new_i = (i + k) % len(nums)
+#         out[new_i] = nums[i]
+#     print(out)
 
+# def reverse_arr(nums):
+#     for i in range(len(nums)//2):
+#         end_i = len(nums) - i - 1
+#         nums[i], nums[end_i] = nums[end_i], nums[i]
+#     return nums
+#
+# # Reversing option O(n), O(1)
+# def rotate_array(nums, k):
+#     nums = reverse_arr(nums)
+#     nums[0:k] = reverse_arr(nums[0:k])
+#     nums[k:] = reverse_arr(nums[k:])
+#     print(nums)
 
 if __name__ == '__main__':
     test_nums = [1,2,3,4,5,6,7]
