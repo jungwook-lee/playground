02f6279adb2d0abc91a22e1d97f7db5a35acc1ae
Jungwook <jungwook.l.92@gmail.com>
2020-01-16 15:39:02 -0500

    Add the final solution

--- python/leetcode/arrays/rotate_array.py
+++ python/leetcode/arrays/rotate_array.py
@@ -28,9 +28,33 @@
 #     nums[k:] = reverse_arr(nums[k:])
 #     print(nums)
 
+# Using cycle dependency method, O(n) time, O(1) space
+def rotate_array(nums, k):
+    n = len(nums)
+    n_swaps = 0
+    i, start = 0, 0
+    while(n_swaps < n):
+        i = start
+        temp = nums[i]
+        while True:
+            next_i = (i + k) % len(nums)
+            nums[next_i], temp = temp, nums[next_i]
+            n_swaps += 1
+            # Handle cycle dependency
+            if next_i == start:
+                break
+            else:
+                i = next_i
+        start += 1
+    print(nums)
+
 if __name__ == '__main__':
     test_nums = [1,2,3,4,5,6,7]
     k = 3
+    rotate_array(test_nums, k)
+
+    test_nums = [1,2,3,4]
+    k = 2
     rotate_array(test_nums, k)
 
     test_nums = [-1, -100, 3, 99]
