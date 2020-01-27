732d24adc7ffc14b7247c4151fe164262b9a2b51
Jungwook <jungwook.l.92@gmail.com>
2020-01-27 15:49:10 -0500

    Finish isSubtree

--- python/leetcode/2020/isSubtree.py
+++ python/leetcode/2020/isSubtree.py
@@ -15,22 +15,21 @@ class Tree(object):
         """ Construct a tree from a heap """
         if not isinstance(x, list):
             raise TypeError('must be a list')
-        self.head = None
-        self.__build(self.head, 0, x)
+        self.head = self.__build(0, x)
 
-    def __build(self, head, i, x):
+    def __build(self, i, x):
         # Exit when list is too big
         if i >= len(x):
-            return 
+            return None
         # Add the new node
         new_node = TreeNode(x[i])
-        head = new_node
-        
         # Insert insert left, right
-        self.__build(new_node.left, (i*2)+1, x)
-        self.__build(new_node.right, (i*2)+2, x)
+        new_node.left = self.__build((i*2)+1, x)
+        new_node.right = self.__build((i*2)+2, x)
+        return new_node
 
     def print_tree(self):
+        """ Simple In-order Print of a Tree object """
         if self.head is None:
             print ("Tree is empty!")
             
@@ -38,7 +37,7 @@ class Tree(object):
             if head is None:
                 return
             else:
-                print(head.val)
+                print(head)
                 print_h(head.left)
                 print_h(head.right)
 
@@ -47,8 +46,26 @@ class Tree(object):
 def isSubtree(self, s, t):
     pass
 
+def isMatch(s,t):
+    if (s is None and t is not None) or (s is not None and t is None):
+        return False
+    elif s is None and t is None:
+        return True
+    return s.val == t.val and isMatch(s.left, t.left) \
+                          and isMatch(s.right, t.right)
+
+def isSubtree(s, t):
+    if isMatch(s, t):
+        return True
+    if not s:
+        return False
+    return isSubtree(s.left, t) or isSubtree(s.right, t)
+
 if __name__ == '__main__':
-    test = [1,2,3,4]
-    new = Tree(test)
-    print(new)
-    new.print_tree()
+    s_in=[4, 1, 2, 4, None, None, None, 1]
+    s = Tree(s_in)
+
+    t_in=[4, 1]
+    t = Tree(t_in)
+
+    print(isSubtree(s.head, t.head))
